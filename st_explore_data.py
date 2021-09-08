ff9eded1a8496258dc2d0fa3f065b2d3bcbb3212
Anton Abilov <anton.abilov@gmail.com>
2021-09-08 18:35:21 -0400

    Added explore tweets in Twitter widget to explore data

--- st_explore_data.py
+++ st_explore_data.py
@@ -2,7 +2,10 @@ import streamlit as st
 import os
 import pandas as pd
 import plotly.express as px
+from dateutil.relativedelta import relativedelta
 import json
+import urllib.parse
+
 
 st.header("Explore cached datasets")
 
@@ -21,3 +24,12 @@ st.write("### Query Params")
 st.write(query_params)
 st.write(f"**Total tweet count:** {df_timeseries.tweet_count.sum():,}")
 st.write(px.line(df_timeseries, x="time_end", y="tweet_count"))
+
+st.subheader("Explore tweets on Twitter for this query")
+date = st.date_input("Select date")
+start_date = date - relativedelta(days=1)
+end_date = date + relativedelta(days=1)
+min_retweets = st.number_input("Minimum retweets", value=0)
+query_str = f"({query_params['query']}) AND since:{start_date} AND until:{end_date} AND min_retweets:{min_retweets}"
+st.write(f"Query: `{query_str}`")
+st.write(f"[Open tweets in Twitter](https://twitter.com/search?q={urllib.parse.quote_plus(query_str)}&src=typed_query&f=top)")
