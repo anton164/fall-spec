d1467bc5de98c2368a17ef84c156eab3b669fbdf
Anton Abilov <anton.abilov@gmail.com>
2021-09-27 08:05:03 -0400

    Added retweet/quote/like/reply count & updated datasets

--- utils/dataset.py
+++ utils/dataset.py
@@ -19,8 +19,14 @@ def create_tweet_df(raw_dataset_tweets):
             "hashtags": [hashtag["tag"] for hashtag in get_with_default(raw_tweet, ["entities", "hashtags"], [])],
             "mentions": [mention["username"] for mention in get_with_default(raw_tweet, ["entities", "mentions"], [])],
             "in_reply_to_user_id": raw_tweet["in_reply_to_user_id"] if "in_reply_to_user_id" in raw_tweet else None,
-            "user_id": raw_tweet["author_id"]
+            "user_id": raw_tweet["author_id"],
+            "retweet_count": raw_tweet["public_metrics"]["retweet_count"],
+            "quote_count": raw_tweet["public_metrics"]["quote_count"],
+            "reply_count": raw_tweet["public_metrics"]["reply_count"],
+            "like_count": raw_tweet["public_metrics"]["like_count"],
+            # TODO
             # to get user screen name we must look at raw_dataset_tweets["users"]
+            
         }
         if "referenced_tweets" in raw_tweet:
             for reference in raw_tweet["referenced_tweets"]:
