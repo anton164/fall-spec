0b6215eb0033779a2243ff98b29e88d345f3b05e
Anton Abilov <anton.abilov@gmail.com>
2021-10-04 13:14:29 -0400

    Updated prepare mstream data to include tweet ids

--- prepare_mstream_data.py
+++ prepare_mstream_data.py
@@ -18,7 +18,7 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-df = pd.read_json(INPUT_DATA_LOCATION + args.input_file)
+df = pd.read_json(INPUT_DATA_LOCATION + args.input_file).set_index("id")
 
 df['text'] = df['text'].apply(lambda x: preprocess_text(x))
 
@@ -58,12 +58,13 @@ df_symbolic.loc[:,'user_id'] =  df_symbolic.loc[:,'user_id'].map(user_id_dict)
 df_symbolic.loc[:,'mentions'] =  df_symbolic.loc[:,'mentions'].map(mentions_dict)
 df_symbolic.loc[:,'text'] =  df_symbolic.loc[:,'text'].map(text_dict)
 
-df_continuous.to_csv(f"{OUTPUT_DATA_LOCATION}{args.output_name}numeric.txt", index=False, header=False)
-df_symbolic.to_csv(f"{OUTPUT_DATA_LOCATION}{args.output_name}categ.txt", index=False, header=False)
+df_continuous.to_csv(f"{OUTPUT_DATA_LOCATION}{args.output_name}_numeric.txt", index=False, header=False)
+df_symbolic.to_csv(f"{OUTPUT_DATA_LOCATION}{args.output_name}_categ.txt", index=False, header=False)
 df_label.to_csv(f"{OUTPUT_DATA_LOCATION}{args.output_name}_label.txt", index=False, header=False)
-df.loc[:,'created_at'].to_csv(f"{OUTPUT_DATA_LOCATION}{args.output_name}time.txt", index=False, header=False)
+df_label.reset_index()[["id"]].to_csv(f"{OUTPUT_DATA_LOCATION}{args.output_name}_tweet_id.txt", index=False, header=False)
+df.loc[:,'created_at'].to_csv(f"{OUTPUT_DATA_LOCATION}{args.output_name}_time.txt", index=False, header=False)
 
-text_file = open(f"{OUTPUT_DATA_LOCATION}{args.output_name}numeric.txt", "w")
+text_file = open(f"{OUTPUT_DATA_LOCATION}{args.output_name}_numeric.txt", "w")
 df_symbolic.shape[0]
 n = text_file.write('\n'*df_symbolic.shape[0])
 text_file.close()
